8ff0da88486be1dd8f09dd971c4a320ee8c17f76
Kishan Kumar <kkishankumar9983@gmail.com>
2026-07-06 04:22:55 +0530

    some changes

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaMailBulk } from "react-icons/fa";
 
 const Footer = () => {
   // Smooth scroll function
@@ -37,16 +37,29 @@ const Footer = () => {
         {/* Social Media Icons - Responsive */}
         <div className="flex flex-wrap justify-center space-x-4 mt-6">
           {[
-            { icon: <FaGithub />, link: "https://github.com/KishanKumarSolanki" },
-            { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/Kishan-Solanki-553b441a4" },
-            
+            {
+              icon: <FaGithub />,
+              link: "https://github.com/KishanKumarSolanki",
+              
+            },
+            {
+              icon: <FaLinkedin />,
+              link: "https://www.linkedin.com/in/Kishan-Solanki-553b441a4",
+              
+            },
+            {
+              icon: <FaMailBulk />,
+              link: "mailto:kkishankumar9983@gmail.com",
+              
+            },
           ].map((item, index) => (
             <a
               key={index}
               href={item.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-xl hover:text-purple-500 transition-transform transform hover:scale-110"
+              target={item.link.startsWith("mailto:") ? undefined : "_blank"}
+              rel={item.link.startsWith("mailto:") ? undefined : "noopener noreferrer"}
+              // title={item.label}
+              className="group relative text-xl hover:text-purple-500 transition-transform transform hover:scale-110"
             >
               {item.icon}
             </a>
